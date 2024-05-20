029f7d20e76009844e4960a07bc594de34800da3
Luis Velasquez <luis_777521@hotmail.com>
2024-05-20 17:57:02 -0600

    Agregue el codigo del problema 3

--- Problema3/problema3.js
+++ Problema3/problema3.js
@@ -1,5 +1,31 @@
 /*  
 A continuación se presenta un código de JavaScript, reescribelo
 utilizando operadores ternarios múltiples ‘?’.
+
+let message;
+
+if (login == 'Trabajador') {
+    message = 'Hola';
+} else if (login == 'Dueño') {
+    message = 'Felicidades';
+}    else if (login == '') {
+    message = 'Sin sesion';
+} else {
+    message ='';
+}
 */
+
+
+// crear el input de texto inicial para poder efectuar la comparacion
+let login = prompt("Ingrese cual es su puesto");
+
+/* usando operadores ternarios multiples se da la instruccion a mostrar
+un mensaje de acuerdo al input que se reciba */
+let message = login == 'Trabajador' ? 'Hola'
+            : login == 'Dueño' ? 'Felicidades'
+            : login == '' ? 'Sin sesion'
+            : '';
+
+// imprimir en consola el resultado de la operacion de acuerdo al input
+console.log(message);
 
