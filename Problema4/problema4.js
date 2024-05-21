2144cdb3c5c08897659b55aa79a9223e09fcd092
Luis Velasquez <luis_777521@hotmail.com>
2024-05-20 19:00:28 -0600

    creacion del codigo .js del problema 4

--- Problema4/problema4.js
+++ Problema4/problema4.js
@@ -12,3 +12,57 @@ incorrecta'.
 ● Para una cadena vacía o una entrada cancelada, se mostrará
 'Cancelado'."
 */
+
+
+// declaracion de variables message y password
+let message;
+let password;
+
+// comando inicial para solicitar el usuario
+let user = prompt("Ingrese su usuario");
+
+// Codigo que determina que accion tomar dependiendo de que se ingrese
+switch (user) {
+    // si ingresa una cadena vacia mostrara cancelado
+    case '':
+        message = 'Cancelado';
+        console.log(message);
+        break;
+    // si presiona la tecla Esc mostrara cancelado
+    case 'Esc':
+        message = 'Cancelado';
+        console.log(message);
+        break;
+    // si ingresa el user Admin procedera a pedir la contaseña
+    case 'Admin':
+        password = prompt("Ingrese su contraseña");
+        // creo otro switch dentro de admin para dar resultado acorde al input
+        switch (password) {
+            // si ingresa Jedi mostrara que la fuerza te acompañe
+            case 'Jedi':
+                message = '¡Que la fuerza te acompañe!';
+                console.log(message);
+                break;
+            // si ingresa una cadena vacia mostrara Cancelado
+            case '':
+                message = 'Cancelado';
+                console.log(message);
+                break;
+            // si el usuario decide cancelar mostrara Cancelado
+            case 'Cancelar':
+                message = 'Cancelado';
+                console.log(message);
+                break;
+            // Si el usuario ingresa cualquier otra contraseña mostrara que es incorrecta
+            default:
+                message = 'Contraseña incorrecta';
+                console.log(message);
+        }
+        break;
+    // si el usuario no ingresa ninguna de las opciones determinadas para el user mostrara no te conozco
+    default:
+        message = 'No te conozco'
+        console.log(message);
+}
+
+// fin de la aplicacion
